139a3f529ceb51672885a73689888ee94e60c5c9
pablo_melo_manoel <pablo.m.manoel@gmail.com>
2024-12-13 10:18:37 -0300

    [step06] - create ec2_public_ip and s3_bucket_arn outputs

--- main.tf
+++ main.tf
@@ -69,3 +69,12 @@ locals {
   }
 }
 
+// step # 6 - output
+
+output "ec2_public_ip" {
+  value = aws_instance.minha_instancia_ec2.public_ip
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.meu-bucket-s3.arn
+}
