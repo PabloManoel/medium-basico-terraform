93418e9f220f62d6b1d0c4031df1ccec633671d4
pablo_melo_manoel <pablo.m.manoel@gmail.com>
2024-12-13 09:14:23 -0300

    [step02] - create resources ec2 instance and s3 bucket

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-// etapa # 1 - configuracao providers
+// step # 1 - provider
 
 terraform {
   required_providers {
@@ -14,3 +14,26 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+// step # 2 - resource
+
+resource "aws_instance" "minha_instancia_ec2" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-custom-instance"
+    contato_suporte = "suporte@sextatech.com"
+    contexto        = "sexta-tech"
+  }
+}
+
+resource "aws_s3_bucket" "meu-bucket-s3" {
+  bucket = "sexta-tech-example"
+
+  tags = {
+    contato_suporte = "suporte@sextatech.com"
+    contexto        = "sexta-tech"
+  }
+}
+
